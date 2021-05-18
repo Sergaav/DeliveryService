ca7fd818837d96535043c4c747445b12b286ac02
Sergaav <sergaav@gmail.com>
2021-05-19 01:19:43 +0300

    Commit little changes

---
+++ sql/SQLUserAdmin.sql
@@ -0,0 +1,4 @@
+INSERT INTO users (id,login,password,first_name,last_name,locale,role) values (default,
+"admin","12345","Andrik","Serha","UA",0);
+
+select * from users;
--- sql/acsdav.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO users (id,login,password,first_name,last_name,role) values (default,
-"admin","12345","Andrik",
