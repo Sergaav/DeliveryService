b00eba082be4be37e8cb93a39f35d2c5f0898bc7
Sergaav <sergaav@gmail.com>
2021-05-19 21:32:40 +0300

    Commit ConnectionPoolHolder

---
+++ src/main/java/com/savaz/delivery/model/dao/impl/ConnectionPoolHolder.java
@@ -0,0 +1,30 @@
+package com.savaz.delivery.model.dao.impl;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import javax.sql.DataSource;
+
+public class ConnectionPoolHolder {
+    private static volatile DataSource dataSource;
+
+    public static DataSource getDataSource() {
+        if (dataSource == null) {
+            synchronized (ConnectionPoolHolder.class) {
+                if (dataSource == null) {
+                    BasicDataSource ds = new BasicDataSource();
+                    ds.setUrl("jdbc:mysql://localhost:3306/delivery");
+                    ds.setUsername("root");
+                    ds.setPassword("12345");
+                    ds.setMinIdle(5);
+                    ds.setMaxIdle(10);
+                    ds.setMaxOpenPreparedStatements(100);
+                    dataSource = ds;
+                }
+            }
+        }
+        return dataSource;
+
+    }
+}
+
+
+
